855a9d5db452dc1d214358b07438925bd2a3b5b2
Ryan Cloherty <rcloherty@cogitocorp.com>
2023-04-10 09:51:41 -0400

    Add @run-at document-start to improve load time
    Added @run-at document-start to the metadata to improve load time. Before this, you'd have to wait for the Jira/Confluence landing page to load before you are taken where back to where you were. Now, you're redirected to where you want to go as soon as the browser reaches the landing page.

--- src/atlast-okta.user.js
+++ src/atlast-okta.user.js
@@ -7,6 +7,7 @@
 // @include      https://confluence.*.tld/*
 // @include      https://jira.*.tld/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=atlassian.com
+// @run-at       document-start
 // ==/UserScript==
 
 /*
