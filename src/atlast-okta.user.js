257a6f8f7aea80de00966ed77e090c34dff1b879
Ryan Cloherty <rcloherty@cogitocorp.com>
2023-05-23 10:04:05 -0400

    Revert "Add @run-at document-start to improve load time (#9)"
    This reverts commit 34219ad8422b6beda4b64e1e9d42af221cd2b803.

--- src/atlast-okta.user.js
+++ src/atlast-okta.user.js
@@ -1,13 +1,12 @@
 // ==UserScript==
 // @name         At last! Okta! (Confluence and Jira Okta Redirect Fixer)
 // @namespace    https://github.com/IncPlusPlus/atlast-okta
-// @version      0.8
+// @version      0.7
 // @description  When Confluence or Jira's sessions expire, they require the user to log in again. However, our Okta configuration doesn't send the browser back to the original page that was being viewed. This userscript fixes that.
 // @author       IncPlusPlus
 // @include      https://confluence.*.tld/*
 // @include      https://jira.*.tld/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=atlassian.com
-// @run-at       document-start
 // ==/UserScript==
 
 /*
