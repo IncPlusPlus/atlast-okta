aef3c8c3859c269462ad482e1feafb40c0bb3d3a
Ryan Cloherty <rcloherty@cogitocorp.com>
2023-06-02 11:20:21 -0400

    Set new URLs

--- dist/atlast-okta.meta.js
+++ dist/atlast-okta.meta.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name         At last! Okta! (Confluence and Jira Okta Redirect Fixer)
 // @namespace    https://github.com/IncPlusPlus/atlast-okta
-// @version      0.9
+// @version      0.10.0
 // @description  When Confluence or Jira's sessions expire, they require the user to log in again. However, our Okta configuration doesn't send the browser back to the original page that was being viewed. This userscript fixes that.
 // @author       IncPlusPlus
 // @include      https://confluence.*.tld/*
 // @include      https://jira.*.tld/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=atlassian.com
+// @downloadUrl  https://github.com/IncPlusPlus/atlast-okta/releases/latest/download/atlast-okta.user.js
+// @updateUrl    https://github.com/IncPlusPlus/atlast-okta/releases/latest/download/atlast-okta.meta.js
 // ==/UserScript==
